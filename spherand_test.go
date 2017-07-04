7832b28240402d9132f5752a8e5812f76dd9c561
Michael McLoughlin <mmcloughlin@gmail.com>
2017-07-04 12:37:23 -0700

    basic range tests

--- spherand_test.go
+++ spherand_test.go
@@ -1,10 +1,33 @@
 package spherand
 
 import (
+	"math"
 	"math/rand"
 	"testing"
 )
 
 func TestUniformSamplerInterface(t *testing.T) {
 	var _ UniformSampler = new(rand.Rand)
+}
+
+func AssertRange(t *testing.T, x, min, max float64) {
+	if x < min || x > max {
+		t.Errorf("value outside allowed range: got %f expected [%f, %f]", x, min, max)
+	}
+}
+
+func TestSphericalRange(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		azimuth, polar := Spherical()
+		AssertRange(t, azimuth, 0, 2*math.Pi)
+		AssertRange(t, polar, 0, math.Pi)
+	}
+}
+
+func TestGeographicalRange(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		lat, lng := Geographical()
+		AssertRange(t, lat, -90, 90)
+		AssertRange(t, lng, -180, 180)
+	}
 }
